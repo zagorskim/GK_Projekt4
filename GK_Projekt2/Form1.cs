95fda175257573c5835be4951eddce30aebbc8ff
Mateusz Zagórski <01161597@pw.edu.pl>
2022-11-03 00:38:27 +0100

    Testing open source *.obj Parser (ObjParser) (successfull)

--- GK_Projekt2/Form1.cs
+++ GK_Projekt2/Form1.cs
@@ -1,3 +1,7 @@
+using ObjParser;
+
+// Parsing *.obj files (ObjParser) taken from some open source implementation (https://github.com/stefangordon/ObjParser.git) in order to quicken up the process of doing this project :)
+
 namespace GK_Projekt2
 {
     public partial class Form1 : Form
@@ -5,6 +9,20 @@ namespace GK_Projekt2
         public Form1()
         {
             InitializeComponent();
+            ReadObjFile();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void ReadObjFile()
+        {
+            // Working!
+            var reader = new Obj();
+            reader.LoadObj(AppDomain.CurrentDomain.BaseDirectory + @"/Resources/quad_sphere.obj");
+            System.Diagnostics.Debug.WriteLine(reader.VertexList);
         }
     }
 }
