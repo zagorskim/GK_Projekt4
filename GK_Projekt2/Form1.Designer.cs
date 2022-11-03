b77dea0492898d566cdeb56f92a231f4018275a3
Mateusz Zagórski <01161597@pw.edu.pl>
2022-11-04 00:46:56 +0100

    First app functionalities including drawing mesh from .obj file

--- GK_Projekt2/Form1.Designer.cs
+++ GK_Projekt2/Form1.Designer.cs
@@ -30,34 +30,110 @@ namespace GK_Projekt2
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.tlpMain = new System.Windows.Forms.TableLayoutPanel();
+            this.flpMenu = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnFillMesh = new System.Windows.Forms.Button();
+            this.pbCanvas = new System.Windows.Forms.PictureBox();
+            this.tlpMain.SuspendLayout();
+            this.flpMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCanvas)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // btnImport
             // 
-            this.button1.Location = new System.Drawing.Point(350, 37);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnImport.Location = new System.Drawing.Point(5, 5);
+            this.btnImport.Margin = new System.Windows.Forms.Padding(5);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(66, 77);
+            this.btnImport.TabIndex = 0;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // tlpMain
+            // 
+            this.tlpMain.ColumnCount = 10;
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMain.Controls.Add(this.flpMenu, 0, 0);
+            this.tlpMain.Controls.Add(this.pbCanvas, 0, 1);
+            this.tlpMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpMain.Location = new System.Drawing.Point(0, 0);
+            this.tlpMain.Name = "tlpMain";
+            this.tlpMain.RowCount = 2;
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
+            this.tlpMain.Size = new System.Drawing.Size(800, 888);
+            this.tlpMain.TabIndex = 1;
+            // 
+            // flpMenu
+            // 
+            this.flpMenu.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.flpMenu.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tlpMain.SetColumnSpan(this.flpMenu, 10);
+            this.flpMenu.Controls.Add(this.btnImport);
+            this.flpMenu.Controls.Add(this.btnFillMesh);
+            this.flpMenu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flpMenu.Location = new System.Drawing.Point(0, 0);
+            this.flpMenu.Margin = new System.Windows.Forms.Padding(0);
+            this.flpMenu.Name = "flpMenu";
+            this.flpMenu.Size = new System.Drawing.Size(800, 88);
+            this.flpMenu.TabIndex = 3;
+            // 
+            // btnFillMesh
+            // 
+            this.btnFillMesh.Location = new System.Drawing.Point(81, 5);
+            this.btnFillMesh.Margin = new System.Windows.Forms.Padding(5);
+            this.btnFillMesh.Name = "btnFillMesh";
+            this.btnFillMesh.Size = new System.Drawing.Size(70, 77);
+            this.btnFillMesh.TabIndex = 1;
+            this.btnFillMesh.Text = "Fill Mesh";
+            this.btnFillMesh.UseVisualStyleBackColor = true;
+            this.btnFillMesh.Click += new System.EventHandler(this.btnFillMesh_Click);
+            // 
+            // pbCanvas
+            // 
+            this.tlpMain.SetColumnSpan(this.pbCanvas, 10);
+            this.pbCanvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbCanvas.Location = new System.Drawing.Point(0, 88);
+            this.pbCanvas.Margin = new System.Windows.Forms.Padding(0);
+            this.pbCanvas.Name = "pbCanvas";
+            this.pbCanvas.Size = new System.Drawing.Size(800, 800);
+            this.pbCanvas.TabIndex = 2;
+            this.pbCanvas.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(800, 888);
+            this.Controls.Add(this.tlpMain);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.tlpMain.ResumeLayout(false);
+            this.flpMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbCanvas)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private Button button1;
+        private Button btnImport;
+        private TableLayoutPanel tlpMain;
+        private FlowLayoutPanel flpMenu;
+        private Button btnFillMesh;
+        private PictureBox pbCanvas;
     }
 
 }
--- GK_Projekt2/Form1.cs
+++ GK_Projekt2/Form1.cs
@@ -1,28 +1,99 @@
 using ObjParser;
+using System.Drawing;
+using System.Windows.Forms;
 
-// Parsing *.obj files (ObjParser) taken from some open source implementation (https://github.com/stefangordon/ObjParser.git) in order to quicken up the process of doing this project :)
+// Parsing *.obj files (ObjParser) taken from some open source implementation (https://github.com/stefangordon/ObjParser.git)
+// in order to quicken up the process of doing this project :) (also extended it with some other functionalities that I needed)
 
 namespace GK_Projekt2
 {
     public partial class Form1 : Form
     {
+        private Obj _loadedObject { get; set; }
+        private Bitmap _bitmap { get; set; }
+
         public Form1()
         {
             InitializeComponent();
-            ReadObjFile();
+            _bitmap = new Bitmap(pbCanvas.Width, pbCanvas.Height);
+            _loadedObject = ReadObjFile(AppDomain.CurrentDomain.BaseDirectory + @"/Resources/quad_sphere.obj");
+            //pbCanvas.Image = _bitmap;
+            //Draw();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        public void ReadObjFile()
+        public Obj ReadObjFile(string path)
         {
             // Working!
             var reader = new Obj();
-            reader.LoadObj(AppDomain.CurrentDomain.BaseDirectory + @"/Resources/quad_sphere.obj");
-            System.Diagnostics.Debug.WriteLine(reader.VertexList);
+            reader.LoadObj(path);
+            return reader;
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            var obj = new Obj();
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                dialog.InitialDirectory = "c:\\";
+                dialog.Filter = "obj files (*.obj)|*.obj";
+                dialog.FilterIndex = 2;
+                dialog.RestoreDirectory = true;
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    var filePath = dialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    obj.LoadObj(dialog.OpenFile());
+                }
+            }
+            _loadedObject = obj;
+            Draw();
+        }
+
+        private void btnFillMesh_Click(object sender, EventArgs e)
+        {
+            // Filling triangles with the proper colouring to be implemented
+            FillMesh();
+            Draw();
+        }
+
+        public void Draw()
+        {
+            var newBitmap = new Bitmap(pbCanvas.Width + 2, pbCanvas.Height + 2);
+            //foreach (var i in _loadedObject.NVList)
+            //{
+            //    var temp = ScaleToCurrentSize(i.X, i.Y);
+            //    newBitmap.SetPixel(temp.Item1, temp.Item2, Color.FromArgb(255, 0, 0));
+            //}
+            foreach (var i in _loadedObject.FaceList)
+                using (var graphics = Graphics.FromImage(newBitmap))
+                {
+                    Pen blackPen = new Pen(Color.Black, 1);
+                    for (int j = 0; j < i.VertexIndexList.Length; j++)
+                    {
+                        var point1 = ScaleToCurrentSize(_loadedObject.VertexList[i.VertexIndexList[j] - 1].X, _loadedObject.VertexList[i.VertexIndexList[j] - 1].Y);
+                        var point2 = ScaleToCurrentSize(_loadedObject.VertexList[i.VertexIndexList[(j + 1) % i.VertexIndexList.Length] - 1].X, _loadedObject.VertexList[i.VertexIndexList[(j + 1) % i.VertexIndexList.Length] - 1].Y);
+                        graphics.DrawLine(blackPen, point1.Item1, point1.Item2,point2.Item1,point2.Item2);
+                    }
+                    blackPen.Dispose();
+                }
+            pbCanvas.Image = newBitmap.Clone(new Rectangle(0, 0, newBitmap.Width, newBitmap.Height), System.Drawing.Imaging.PixelFormat.DontCare);
+        }
+
+        public void DrawLine()
+        {
+
+        }
+
+        public void FillMesh()
+        {
+
+        }
+
+        public (int, int) ScaleToCurrentSize(double x, double y)
+        {
+            return ((int)((x + 1) * pbCanvas.Width / 2), (int)((y + 1) * pbCanvas.Height / 2));
         }
     }
 }
